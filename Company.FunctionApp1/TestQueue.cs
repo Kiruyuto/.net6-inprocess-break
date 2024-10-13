92316ed583971ecdfaa9907a51e1155837acc04f
Hubert@Home <h.zielinski.business@gmail.com>
2024-10-13 14:34:36 +0200

    feat: Add files

---
+++ Company.FunctionApp1/TestQueue.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Company.FunctionApp1;
+
+public static class TestQueue
+{
+    [FunctionName("TestQueue")]
+    public static async Task RunAsync([QueueTrigger("myqueue", Connection = "AzureQueueConnectionTest")] string myQueueItem, ILogger log)
+    {
+        log.LogInformation("C# Queue trigger function processed: {MyQueueItem}", myQueueItem);
+        await Task.Delay(5 * 1_000);
+    }
+}
---
+++ Company.FunctionApp1/TestTimer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Company.FunctionApp1;
+
+public class TestTimer
+{
+    [FunctionName("TestTimer")]
+    public async Task RunAsync([TimerTrigger("0 */5 * * * *", RunOnStartup = true)] TimerInfo myTimer, ILogger log)
+    {
+        log.LogInformation("C# Timer trigger function executed at: {UtcNow}", DateTime.UtcNow);
+        await Task.Delay(5 * 1_000);
+    }
+}
